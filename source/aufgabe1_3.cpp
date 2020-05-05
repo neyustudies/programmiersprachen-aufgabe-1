29298b91b6c6556ff81a7b58e1826eac5572cabd
neyustudies <science-cat@gmx.net>
2020-05-05 21:14:22 +0200

    first commit aufgabe 1.3

---
+++ source/aufgabe1_3.cpp
@@ -0,0 +1,31 @@
+/* 
+ * Lisa P.
+ * Aufgabe 1.3
+ * Berechnung der kleinsten Zahl,
+ * die durch die Zahlen 1 bis 20 teilbar ist
+ */
+
+#include <iostream>
+
+
+// erweiterter euklidischer Algorithmus
+int eeA(int a, int b) {
+   if (b == 0)
+      return a;
+   return eeA(b, a % b);
+}
+
+// kleinstes gemeinsames Vielfaches
+long kgV(long n) {
+    long var = 1;
+    for (long i = 1; i <= n; i++)
+        var = (var * i)/(eeA(var, i));
+    return var;
+}
+   
+int main() {
+    long n = 20;
+    std::cout << kgV(n) << std::endl;
+    
+    return 0;
+}
