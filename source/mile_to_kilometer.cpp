93ddddc1dd68f5982c529830565f689121481438
Lisa <51492846+neyustudies@users.noreply.github.com>
2020-05-06 15:46:01 +0200

    new commit mile_to_kilometer

---
+++ source/mile_to_kilometer.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Lisa P.
+ * Programmierung Aufgabe 1.15
+ * Entwicklung mit TDD: Funktion, welche Meilen in Kilometer umrechnet
+ */
+ 
+
+#define CATCH_CONFIG_RUNNER
+#include <iostream>
+#include "catch.hpp"
+
+
+double mile_to_kilometer(double mile){
+	double km = mile*1.609344;
+	return km;	
+}
+
+int main(){
+	double mile;
+	std::cout << "Bitte geben Sie die umzurechnende Meilenzahl ein: ";
+	std::cin >> mile;
+    std::cout << "wird berechnet..." << std::endl;
+	std::cout << mile << " Meilen sind umgerechnet " << mile_to_kilometer(mile) << " Kilometer." << std::endl;
+
+	return 0;
+}
