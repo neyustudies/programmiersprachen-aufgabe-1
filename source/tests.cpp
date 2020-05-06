23647ac256cc27bed267e0715075eedba99b3d51
neyustudies <science-cat@gmx.net>
2020-05-06 22:50:06 +0200

    tests 1.8

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,23 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+int gcd (int a, int b) {
+	int c = 0;
+	while (b != 0) {
+		c = a%b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+
+TEST_CASE("describe_gcd", "[gcd]") {
+  REQUIRE(gcd(2,4) == 2); 
+  REQUIRE(gcd(9,6) == 3); 
+  REQUIRE(gcd(3,7) == 1);
+}
+
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
