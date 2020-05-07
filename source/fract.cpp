721b31cdc5bd7308ada93a3f735348309ded9295
neyustudies <science-cat@gmx.net>
2020-05-07 21:00:33 +0200

    Create fract.cpp

---
+++ source/fract.cpp
@@ -0,0 +1,23 @@
+/* 
+ * Lisa P.
+ * Programmierung Aufgabe 1.11
+ * Berechnung des Nachkomma-Anteils einer Gleitkommazahl
+ * Entwicklung mit TDD
+ */
+
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+double fract(double d) {
+    int a = d;
+    double erg = d - (double)a;
+    return erg;
+
+}
+
+TEST_CASE("describe_fract", "[fract]") {
+  REQUIRE(fract(2.23442) == Approx(0.23442)); 
+  REQUIRE(fract(91.211113) == Approx(0.211113)); 
+  REQUIRE(fract(3.307) == Approx(0.307));
+  REQUIRE(fract(-4.0) == Approx(0));
+  REQUIRE(fract(-33.4303857548767) == Approx(-0.4303857548767));
